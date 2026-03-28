be0777deb7820038e67f9f7fb7abc36b52495186
andreabrantes80 <meloabrantes@gmail.com>
2026-03-28 15:18:22 -0300

    add function de delete item

--- src/components/HistoricoList/index.js
+++ src/components/HistoricoList/index.js
@@ -1,20 +1,37 @@
 
+import { Alert, TouchableWithoutFeedback } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { Container, IconView, Tipo, TipoText, ValorText } from "./styles";
 
-export default function HistoricoList({data}) {
-    return (
-        <Container>
-            <Tipo>
-                <IconView tipo={data.type} >
-                    <Icon name={data.type === 'despesa' ? 'arrow-down' : 'arrow-up'}
-                        size={20}
-                        color='#fff' />
-                    <TipoText>{data.type }</TipoText>
-                </IconView>
-            </Tipo>
+export default function HistoricoList({ data, deleteItem }) {
 
-            <ValorText>R$ { data.value}</ValorText>
-        </Container>
+    function handleDeleteItem() {
+        Alert.alert(
+            'Atenção',
+            'Você tem certeza que deseja deletar esse registro?',
+            [{
+                text: 'Cancelar',
+                style: 'cancel'
+            }, {
+                text: 'Continuar',
+                onPress: ()=> deleteItem(data.id)
+            }]
+        )
+    }
+    return (
+        <TouchableWithoutFeedback onLongPress={handleDeleteItem}>
+            <Container>
+                <Tipo>
+                    <IconView tipo={data.type} >
+                        <Icon name={data.type === 'despesa' ? 'arrow-down' : 'arrow-up'}
+                            size={20}
+                            color='#fff' />
+                        <TipoText>{data.type}</TipoText>
+                    </IconView>
+                </Tipo>
+
+                <ValorText>R$ {data.value}</ValorText>
+            </Container>
+        </TouchableWithoutFeedback>
     )
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -50,7 +50,23 @@ export default function Home() {
         getMovements();
 
         return () => isActive = false;
-    }, [isFocused])
+    }, [isFocused, dateMovements])
+
+    async function handleDelete(id) {
+        try {
+            await api.delete('/receives/delete', {
+                params: {
+                    item_id: id
+                }
+            })
+
+            setDateMovements(new Date())
+
+        } catch (err) {
+            console.log(err)
+       }
+
+    }
 
 
     return (
@@ -75,7 +91,7 @@ export default function Home() {
             <List
                 data={movements}
                 keyExtractor={item => item.id}
-                renderItem={({ item }) => <HistoricoList data={item } />}
+                renderItem={({ item }) => <HistoricoList data={item } deleteItem={handleDelete} />}
                 showsVerticalScrollIndicator={false}
                 contentContainerStyle={{paddingBottom: 20}}
             />
